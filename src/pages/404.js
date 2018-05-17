4cdfea315b45ac493f9b27b02c17ea56558dbef7
brad <brad@sixtostart.com>
2018-05-17 15:20:17 +0100

    first commit  🥕

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => (
+  <div>
+    <h2>404</h2>
+      <p>Nothing here :/.</p>
+      <Link to="/">Head home?</Link>
+  </div>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <p>Still learning.</p>
+    <p>Contact: <a href="mailto:brad@prtr.co">brad@prtr.co</a></p>
+  </div>
+);
